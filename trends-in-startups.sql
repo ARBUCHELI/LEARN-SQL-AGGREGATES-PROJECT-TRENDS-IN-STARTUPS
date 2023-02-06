c5e857fd2b5a71b3958bcbc3d572c16855451bc7
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-06 17:21:44 -0500

    first commit

---
+++ trends-in-startups.sql
@@ -0,0 +1,13 @@
+CREATE TABLE startups (
+    name TEXT,
+    location TEXT,
+    category TEXT, 
+    employees INTEGER,
+    raised INTEGER,
+    valuation INTEGER,
+    founded INTEGER,
+    stage TEXT, 
+    ceo TEXT, 
+    info TEXT
+)
+
